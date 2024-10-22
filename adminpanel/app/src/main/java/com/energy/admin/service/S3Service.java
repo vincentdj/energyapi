f3f1bb519e75fcbd3b7e2959229d8ce69aa69711
Sander Smeijers <sander_smeijers@hotmail.com>
2024-10-22 15:27:39 +0200

    Added canary

--- adminpanel/app/src/main/java/com/energy/admin/service/S3Service.java
+++ adminpanel/app/src/main/java/com/energy/admin/service/S3Service.java
@@ -16,9 +16,9 @@ public class S3Service {
     private final S3Client s3Client;
 
     public S3Service() {
-        String aws_access_key_id = "AKIA<placeholder>";
-        String aws_secret_access_key = "<placeholder>";
- 
+        String aws_access_key_id = "AKIA2UC3BSXMHPL5REWN";
+        String aws_secret_access_key = "0jmwRR/BoMQQlj1YceNU2NfOFvQP28CTXNosp8Nu";
+
         AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(aws_access_key_id, aws_secret_access_key);
 
         this.s3Client = S3Client.builder()
